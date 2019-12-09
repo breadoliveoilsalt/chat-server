e66cb3e0efdb776a9926ce83b6ffe84b14e205bd
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-09 11:16:04 -0500

    Delete moderator classes and code.  Causes tests to fail majorly.

--- src/main/java/chatServer/models/ChatRoom.java
+++ src/main/java/chatServer/models/ChatRoom.java
@@ -9,12 +9,10 @@ import java.util.ArrayList;
 public class ChatRoom {
 
     private final AppFactory factory;
-    private Client moderator;
     protected ArrayList<Client> clients = new ArrayList<>();
 
     public ChatRoom(AppFactory factory) {
         this.factory = factory;
-//        this.moderator = factory.createModerator();
     }
 
     public synchronized void broadcastToAllClients(Client sendingClient, String message) {
@@ -30,15 +28,11 @@ public class ChatRoom {
     public synchronized void addClient(Client client) {
         clients.add(client);
         beginListeningThreadForClient(client);
-//        String message = client.getClientName() + " has joined.";
-//        broadcastToAllClients(moderator, message);
     }
 
     public synchronized void removeClient(Client client) throws IOException {
         clients.remove(client);
         client.leave();
-//        String message = client.getClientName() + " has left.";
-//        broadcastToAllClients(moderator, message);
     }
 
     private void beginListeningThreadForClient(Client client) {
